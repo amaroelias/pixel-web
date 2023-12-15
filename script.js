fad7b08ac010fac41ef4c9966ef8ddb2ac8f5f1e
Amaro Elias <amaronascimento2016@hotmail.com>
2023-12-14 22:40:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function irParaSelecaoDeFases() {
+    window.location.href = "selecao-fases.html";
+}
+
+function irParaAjuda() {
+    window.location.href = "ajuda.html";
+}
+
+function irParaSobre() {
+    window.location.href = "sobre.html";
+}
+
+function irParaFase1() {
+    window.location.href = "fase1.html";
+}
+
+function retornarSelecaoFases() {
+    window.location.href = "selecao-fases.html";
+}
